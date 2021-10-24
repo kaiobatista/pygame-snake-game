b20ea0c8a5a54951e6ecbe600adbbbe52c7c82b0
KaioBatista <kaiobatist@gmail.com>
2021-10-24 19:28:57 -0300

    :bug:fix a bug

--- settings.py
+++ settings.py
@@ -1,16 +1,16 @@
 import os
-
+ABS_PATH = os.path.dirname(os.path.abspath(__file__))
 
 WIDTH, HEIGHT = 600, 600
 TITLE = 'Snake Game'
 
-GAME_MUSIC = os.getcwd() + "/src/snd/8bit-bossa.mp3"
-APPLE_SOUND = os.getcwd() + "/src/snd/apple_sound.wav"
-GAME_OVER_SOUND = os.getcwd() + "/src/snd/game-over-sound.wav"
+GAME_MUSIC = ABS_PATH + "/src/snd/8bit-bossa.mp3"
+APPLE_SOUND = ABS_PATH + "/src/snd/apple_sound.wav"
+GAME_OVER_SOUND = ABS_PATH + "/src/snd/game-over-sound.wav"
 
-GAME_OVER_MUSIC = os.getcwd() + "/src/snd/game-over.ogg"
-START_SCREEN_MUSIC = os.getcwd() + "/src/snd/menu.mp3"
+GAME_OVER_MUSIC = ABS_PATH + "/src/snd/game-over.ogg"
+START_SCREEN_MUSIC = ABS_PATH + "/src/snd/menu.mp3"
 
-DEFAULT_FONT = os.getcwd() + "/src/fnts/nasalization-rg.ttf"
+DEFAULT_FONT = ABS_PATH + "/src/fnts/nasalization-rg.ttf"
 
-HIGHSCORE_PATH = os.getcwd() + "/highscore.txt"
+HIGHSCORE_PATH = ABS_PATH + "/highscore.txt"
